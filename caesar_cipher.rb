0adedff3d7202b6e30a8297ba63e4cb580f83d26
alidauda <alidauda14@gmail.com>
2024-06-24 14:24:31 +0100

    first commit

---
+++ caesar_cipher.rb
@@ -0,0 +1,25 @@
+
+def caesar_cipher(value,step)
+  newArray=Array.new(0)
+  letters={"a"=>1,"b"=>2,"c"=>3,"d"=>4,"e"=>5,"f"=>6,"g"=>7,"h"=>8,"i"=>9,"j"=>10,"k"=>11,"l"=>12,"m"=>13,"n"=>14,
+    "o"=>15,"p"=>16,"q"=>17,"r"=>18,"s"=>19,"t"=>20,"u"=>21,"v"=>22,"w"=>23,"x"=>24,"y"=>25,"z"=>26}
+ newValue=value.split("")
+ newValue.each  do |item|
+is_upperCase=item==item.upcase
+if letters.has_key?(item.downcase)
+  single_letter=letters[item.downcase]
+  number=single_letter+5
+  d_letter=letters.key(number%26)
+  if is_upperCase
+    newArray.push(d_letter.upcase)
+else
+  newArray.push(d_letter)
+  end
+ else
+  newArray.push(item)
+end
+ end
+puts newArray.join("")
+end
+
+caesar_cipher("Ali", 5)
---
+++ csv_reader.rb
@@ -0,0 +1,27 @@
+require "CSV"
+require_relative "book_in_stock"
+
+class CsvReader
+  attr_reader :book_in_stock
+  def initialize
+    @books_in_stock=[]
+  end
+  def read_in_csv_data(csv_file_name)
+    CSV.foreach(csv_file_name,headers:true) do |row|
+      @books_in_stock<< BookInStock.new(row['ISBN'],row['Price'])
+    end
+end
+  def total_value_in_stock
+    sum = 0.0
+    @books_in_stock.each { |book| sum += book.price_in_cents }
+    sum / 100.0
+  # ...
+  end
+  def number_of_each_isbn
+  # ...
+  end
+
+end
+reader=CsvReader.new
+reader.read_in_csv_data("test.csv")
+puts reader.total_value_in_stock
